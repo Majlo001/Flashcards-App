3be0a7ad785d613f588da6034de9ab95fe7bef22
Majlo001 <szmilosz01@gmail.com>
2024-02-16 19:36:12 +0100

    initial commit

---
+++ client/flashcards-nextjs-app/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Example page in Next.js!</h1>
+      <p>This is an example main page.</p>
+    </div>
+  );
+};
+
+export default HomePage;
